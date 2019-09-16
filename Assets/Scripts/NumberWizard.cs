eddf4eb2997da1ecaefd687d2e10ca2d51b91360
Kevin Nobles <nobles-k@hotmail.com>
2019-09-16 22:43:08 +0200

    Initial commit

---
+++ Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberWizard : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int max = 1000;
+        int min = 1;
+        Debug.Log("Minimum value is " + min);
+        Debug.Log("Maximum value is " + max);
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Debug.Log("Pressed UpArrow");
+        }
+    }
+}
